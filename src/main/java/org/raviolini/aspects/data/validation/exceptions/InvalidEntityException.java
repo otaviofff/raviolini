c23642397486c57667c5492271c4fb7f8ebfb948
Otavio Ferreira <otavio.ferreira@gmail.com>
2015-11-10 22:37:06 -0200

    Added inner message to validation exception.

--- src/main/java/org/raviolini/aspects/data/validation/exceptions/InvalidEntityException.java
+++ src/main/java/org/raviolini/aspects/data/validation/exceptions/InvalidEntityException.java
@@ -3,4 +3,8 @@ package org.raviolini.aspects.data.validation.exceptions;
 public class InvalidEntityException extends Exception {
 
     private static final long serialVersionUID = 6312612680944279274L;
+    
+    public InvalidEntityException() {
+        super("Invalid entity.");
+    }
 }
